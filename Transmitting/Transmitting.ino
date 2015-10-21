6bdb4c7b3eb39a8f222fb38de0ab8021b82a9e0d
Paul Barker <paul@thebarkers.com>
2015-10-20 20:11:43 -0600

    Committed to Version 3
    Updated transmitting to version 3

--- Transmitting/Transmitting.ino
+++ Transmitting/Transmitting.ino
@@ -11,13 +11,28 @@
 SoftwareSerial xbee(2,3);
 
 
+// Time without receiving a message until determining no connection (1 sec = 1000)
+long timeout = 1000;
+
+// Interval time before retransmitting the same value
+int interval = 500;
+
 // Potentiometer values from joysticks and joystick buttons
 int pot1x;
 int pot1y;
-int pot2x;
 int pot2y;
-boolean joy1;
-boolean joy2;
+int joy1;
+
+// Older values of joysticks
+int pot1xOld;
+int pot1yOld;
+int pot2yOld;
+int joy1Old;
+
+// Last potentiometer change
+long pot1xLC = 0 - interval;
+long pot1yLC = 0 - interval;
+long pot2yLC = 0 - interval;
 
 // Motor max and min values to scale potentiometer values
 int motMin = 0;
@@ -25,13 +40,12 @@ int motMax = 180;
 
 // Time variables for determining if there's a connection
 long time;
-long lastMessage;
-
-// Time without receiving a message until determining no connection (1 sec = 1000)
-long timeout = 5000;
+long lastMessage = 0;
+long lastButton = 0;
 
 // Boolean to determine if conencted to boat
 boolean isConnected = false;
+boolean beastMode = false;
 
 // Value to store received data
 unsigned int value;
@@ -44,12 +58,14 @@ void setup() {
   // Setup pins for input
   pinMode(A0, INPUT);  // pot1x
   pinMode(A1, INPUT);  // pot1y
-  pinMode(A2, INPUT);  // pot2x
+  //pinMode(A2, INPUT);// pot2x
   pinMode(A3, INPUT);  // pot2y
-  pinMode(4, INPUT);   // joy1
-  pinMode(5, INPUT);   // joy2
+  pinMode(4, INPUT);   
+  //pinMode(5, INPUT); // joy2
+  pinMode(10, OUTPUT);
   pinMode(11, OUTPUT); // LEDs for displaying connection status
   pinMode(12, OUTPUT); 
+  digitalWrite(4, HIGH);
 }
 
 
@@ -61,7 +77,7 @@ void loop() {
   if (xbee.available()){
     // Read and store recieved value
     value = xbee.parseInt();
-    
+    xbee.flush();
     // Make sure value isn't null (or zero)
     if(value == 420){
       // Get time last recieved a message and set connection status to true
@@ -70,40 +86,84 @@ void loop() {
     }
   }
   
-  
   pot1x = 180-adjustRange(analogRead(A0), 1, 493, 494, 1020);
-  //pot1x = 90;
-  xbee.println(1000+pot1x);
-  Serial.println(pot1x);
+  if(( pot1xOld != pot1x ) || ( abs(time-pot1xLC) > interval )){
+    xbee.println(1000+pot1x);
+    pot1xLC = time;
+    Serial.println(pot1x);
+  }
+  pot1xOld = pot1x;
+  
   
   
   pot1y = adjustRange(analogRead(A1), 1, 505, 505, 1021);
-  xbee.println(2000+pot1y);
+  if(( pot1yOld != pot1y ) || ( abs(time-pot1yLC) > interval )){
+    xbee.println(2000+pot1y);
+    pot1yLC = time;
+  }
+  pot1yOld = pot1y;
   //Serial.println(pot1y);
   
   pot2y = adjustRange(analogRead(A3), 3, 501, 501, 1018);
-  xbee.println(4000+pot2y);
-  Serial.println(pot2y);
+  if(( pot2yOld != pot2y ) || ( abs(time-pot2yLC) > interval )){
+    xbee.println(4000+pot2y);
+    pot2yLC = time;
+  }
+  pot2yOld = pot2y;
+  //Serial.println(pot2y);
   //Serial.println(0);
   
+  joy1 = digitalRead(4);
+  if(joy1 == HIGH && joy1Old == LOW && time - lastButton > 200){
+    if (beastMode)
+      beastMode = false;
+    else
+      beastMode = true;
+    lastButton = millis();
+  }
+  joy1Old = joy1;
+  
+    
   // If last message received is longer than timeout, set connection status to false
   if ((abs(lastMessage-time) > timeout)){
     isConnected = false;
   }
 
-  if(isConnected==true){digitalWrite(12, HIGH);digitalWrite(11, LOW);}
-  else{digitalWrite(12, LOW);digitalWrite(11, HIGH);}
+  if (isConnected) {
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+  } else {
+    digitalWrite(12, LOW);
+    digitalWrite(11, HIGH);
+  }
+  
+  if (beastMode) {
+    digitalWrite(10,HIGH); 
+  } else {
+    digitalWrite(10,LOW);
+  }
+  
+  
   // Delay for accurate pot readings
-  delay(100);
+  delay(150);
 }
 
 int adjustRange(int raw, int low, int midLow, int midHigh, int high){
+   int i;
    if (raw < midLow){
-     return map(raw,low,midLow,motMin,(motMax-motMin)/2);
+     i = map(raw,low,midLow,0,512);
+   } else if (raw > midHigh){
+     i = map(raw,midHigh,high,512,1023);
+   } else {
+     i = 512;
    }
-   else if (raw > midHigh){
-     return map(raw,midHigh,high,(motMax-motMin)/2,motMax);
+   
+   // If beastmode is on, then use full 180 degrees of freedom
+   if (beastMode){
+     return map(i,0,1023,0,180);
    }
-   return (motMax-motMin)/2;
+   
+   // Otherwise, use 90 degrees of freedom
+   return map(i,0,1023,45,135);
 }
 
