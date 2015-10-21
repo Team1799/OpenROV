92a4b3b9d52ec7af32ee84daf7f3c9ec3d5be243
Paul Barker <paul@thebarkers.com>
2015-10-20 20:04:35 -0600

    Changed to version 2
    Updated Receiving and Transmitting to version 2

--- Receiving/Receiving.ino
+++ Receiving/Receiving.ino
@@ -1,37 +1,89 @@
+// Arduino Submarine Project
+// Boat Code
+// Created by: Josh V.
+// 
+// Purpose
+//   Receive values from transmitting arduino and control motors with it
+//
 #include <SoftwareSerial.h>
-#include <Servo.h> 
+#include <Servo.h>
 
+// Setup xbee pins 0.
+SoftwareSerial xbee(2,3);
 
-Servo myservo;
-SoftwareSerial xbee(2,3); // RX, TX
+// Setup servo variables
+Servo left;
+Servo right;
+Servo front;
 
-void setup()  
-{
+// Potentiometer values from joysticks and joystick buttons
+int pot1x;
+int pot1y;
+int pot2x;
+int pot2y;
+boolean joy1;
+boolean joy2;
+
+// Time variables for determining if there's a connection
+long time;
+long lastMessage;
+
+// Time without receiving a message until determining no connection (1 sec = 1000)
+int timeout = 5000;
+
+// Value to store received data
+unsigned int value;
+
+void setup() {
+  // Open up serial and xbee for data transmission
   Serial.begin(9600);
-  //Serial.println("Goodnight moon!");
   xbee.begin(9600);
   
-  myservo.attach(5);
-  //pinMode(5, OUTPUT);
-  //xbee.println("Hello, world?");
+  // Open up servo pins
+  left.attach(5);
+  right.attach(6);
+  front.attach(9);
+  pinMode(12, OUTPUT); 
 }
 
-void loop() // run over and over
-{
-  
+
+void loop() {
+  // Get current time
+  time = millis();
+ 
+  // Wait until a message is received
   if (xbee.available()){
-    int value = xbee.parseInt();
+    // Get time last recieved a message
+    lastMessage = millis();
+    
+    // Read and store recieved value
+    value = xbee.parseInt();
+    xbee.flush();
+    
+    // Make sure value isn't null (or zero)
     if(value!=0){
-      // Subtract the 1 added from transmitting end
-      value -= 1;
-      
-      myservo.write(value);
-      //analogWrite(5,value);
-      Serial.println(value);
+      // Interpret the value using the digit from the thousandth place and assign it to its cooresponding variable
+      if(value/1000 == 1)
+        pot1x = value%1000;
+      if(value/1000 == 2)
+        pot1y = value%1000;
+      if(value/1000 == 4)
+        pot2y = value%1000;
     }
+    xbee.println(420);
+    
+    // Return value to confirm connection
   }
-    //Serial.write(xbee.read());
-  //if (Serial.available())
-  //  xbee.write(Serial.read());
+  
+  // Prevents motor movement unless there is a connection
+  
+  
+  if ((abs(lastMessage-time) < timeout)){
+    front.write(pot1x);
+  }
+  else {
+    front.write(90);
+  }
+  delay(50);
 }
 
--- Transmitting/Transmitting.ino
+++ Transmitting/Transmitting.ino
@@ -1,36 +1,109 @@
+// Arduino Submarine Project
+// Controller Code
+// Created by: Josh V.
+// 
+// Purpose
+//   Receive values from joystick(s) and transmit it to the other Arduino using xbee's
+//
 #include <SoftwareSerial.h>
 
-SoftwareSerial xbee(2,3); // RX, TX
+// Setup xbee pins 
+SoftwareSerial xbee(2,3);
 
-int value = 0;
-boolean isUp = true;
-void setup()  
-{
-  // Open serial communications and wait for port to open:
+
+// Potentiometer values from joysticks and joystick buttons
+int pot1x;
+int pot1y;
+int pot2x;
+int pot2y;
+boolean joy1;
+boolean joy2;
+
+// Motor max and min values to scale potentiometer values
+int motMin = 0;
+int motMax = 180;
+
+// Time variables for determining if there's a connection
+long time;
+long lastMessage;
+
+// Time without receiving a message until determining no connection (1 sec = 1000)
+long timeout = 5000;
+
+// Boolean to determine if conencted to boat
+boolean isConnected = false;
+
+// Value to store received data
+unsigned int value;
+
+void setup() {
+  // Open up serial and xbee for data transmission
   Serial.begin(9600);
-  
-  //Serial.println("Goodnight moon!");
   xbee.begin(9600);
-  //xbee.println("Hello, world?");
+  
+  // Setup pins for input
+  pinMode(A0, INPUT);  // pot1x
+  pinMode(A1, INPUT);  // pot1y
+  pinMode(A2, INPUT);  // pot2x
+  pinMode(A3, INPUT);  // pot2y
+  pinMode(4, INPUT);   // joy1
+  pinMode(5, INPUT);   // joy2
+  pinMode(11, OUTPUT); // LEDs for displaying connection status
+  pinMode(12, OUTPUT); 
 }
 
-void loop() // run over and over
-{
-  //if (xbee.available())
-  //  Serial.write(xbee.read());
-  //if (Serial.available())
-  //  xbee.write(Serial.read());
-  if(isUp){
-    value++; 
-  } else {
-    value--; 
+
+void loop() {
+  // Get current time
+  time = millis();
+  
+  // Wait until a message is received
+  if (xbee.available()){
+    // Read and store recieved value
+    value = xbee.parseInt();
+    
+    // Make sure value isn't null (or zero)
+    if(value == 420){
+      // Get time last recieved a message and set connection status to true
+      lastMessage = millis();
+      isConnected = true;
+    }
   }
-  if(value==120)
-    isUp = false;
-  if(value==0)
-    isUp = true;
-  xbee.println(value+1);
-  Serial.println(value);
-  delay(20);
+  
+  
+  pot1x = 180-adjustRange(analogRead(A0), 1, 493, 494, 1020);
+  //pot1x = 90;
+  xbee.println(1000+pot1x);
+  Serial.println(pot1x);
+  
+  
+  pot1y = adjustRange(analogRead(A1), 1, 505, 505, 1021);
+  xbee.println(2000+pot1y);
+  //Serial.println(pot1y);
+  
+  pot2y = adjustRange(analogRead(A3), 3, 501, 501, 1018);
+  xbee.println(4000+pot2y);
+  Serial.println(pot2y);
+  //Serial.println(0);
+  
+  // If last message received is longer than timeout, set connection status to false
+  if ((abs(lastMessage-time) > timeout)){
+    isConnected = false;
+  }
+
+  if(isConnected==true){digitalWrite(12, HIGH);digitalWrite(11, LOW);}
+  else{digitalWrite(12, LOW);digitalWrite(11, HIGH);}
+  // Delay for accurate pot readings
+  delay(100);
+}
+
+int adjustRange(int raw, int low, int midLow, int midHigh, int high){
+   if (raw < midLow){
+     return map(raw,low,midLow,motMin,(motMax-motMin)/2);
+   }
+   else if (raw > midHigh){
+     return map(raw,midHigh,high,(motMax-motMin)/2,motMax);
+   }
+   return (motMax-motMin)/2;
 }
 
